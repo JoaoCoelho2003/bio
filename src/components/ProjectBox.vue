866d91a968645ded849a420b447c45d5ec294221
jojocoelho <jhcoelho03@gmail.com>
2024-07-11 21:39:15 +0100

    fix: fixed padding

--- src/components/ProjectBox.vue
+++ src/components/ProjectBox.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="bg-[#062656] border border-[#021d44] p-4 rounded-lg shadow-xl flex flex-col items-start overflow-hidden space-y-4 z-10 relative" @click="showPopup = !showPopup">
+  <div class="bg-[#062656] border border-[#021d44] rounded-lg shadow-xl flex flex-col items-start overflow-hidden space-y-2 sm:space-y-4 z-10 relative" @click="showPopup = !showPopup">
     <img :src="imageSrc" alt="Project Image" class="w-full h-90 object-cover rounded-t-lg"/>
 
-    <div class="text-white">
+    <div class="text-white p-1">
       <h3 class="text-xs sm:text-xl font-bold">{{ title }}</h3>
-      <p class="text-gray-300 mt-2 text-[0.675rem] sm:text-base line-clamp-2 sm:line-clamp-3">{{ description }}</p>
+      <p class="text-gray-300 text-[0.675rem] sm:text-base line-clamp-2 sm:line-clamp-3">{{ description }}</p>
     </div>
 
     <div class="flex flex-wrap gap-2 sm:gap-4">
@@ -12,7 +12,7 @@
         <i :class="tech"></i>
       </span>
     </div>
-    <div class="p-1 sm:p-4">
+    <div class="p-4 sm:p-4">
     <a :href="giturl" target="_blank" class="absolute bottom-2 right-2 sm:bottom-4 sm:right-4 text-md md:text-3xl md:font-light text-gray-200 dark:text-gray-300 hover:underline hover:text-[#d62f6a] transition-colors duration-300">
       <i class="bi bi-github"></i>
     </a>
