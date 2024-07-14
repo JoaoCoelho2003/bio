1db3a7fe23b647d1c1c1c090be0b2d10ac9d6a90
jojocoelho <jhcoelho03@gmail.com>
2024-07-14 20:24:31 +0100

    fix: google analytics

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueGtag, {
+    appName: 'portfolio',
+    pageTrackerScreenviewEnabled: true,
+    config: { id: import.meta.env.VUE_APP_GOOGLE_ANALYTICS_ID }
+})
 
 app.mount('#app')
