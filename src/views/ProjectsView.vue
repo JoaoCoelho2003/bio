bb2d261cba10bf54c471e65fad38f85827d3f65b
jojocoelho <jhcoelho03@gmail.com>
2024-10-02 23:04:04 +0100

    refactor: project text and images

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -19,26 +19,26 @@
           <ProjectBox 
             :images= '["/Dharma/dharmaLogo.png"]'
             title="Dharma Network - NFT Marketplace"
-            description="Dharma Network is a decentralized NFT marketplace that allows users to buy and sell NFTs. The platform was developed using Vue.js, typescript, millisearch and tailwindcss."
+            description="Dharma Network is a decentralized NFT marketplace that allows users to buy and sell NFTs."
             :technologies="['devicon-vuejs-plain colored', 'devicon-typescript-original colored', 'devicon-tailwindcss-original colored']"
             weburl = "https://www.mydharma.network/"
           />
           <ProjectBox
             :images= '["/CG/CG1.png", "/CG/CG2.png", "/CG/CG3.png","/CG/CG4.png", "/CG/CG5.png"]'
             title="3D Engine and Generator - Computer Graphics Project"
-            description="This project consists of a 3D engine and generator developed in C++ and OpenGL. The engine is capable of rendering 3D models, applying textures, and performing transformations such as translation, rotation, and scaling. The generator allows users to create 3D models by defining vertices, edges, and faces. The application also includes a lighting system that simulates the effects of ambient, diffuse, and specular light. As part of the project, a demo of the solar system was created, showcasing the engine's capabilities. The engine and generator were developed as part of a Computer Graphics course at the University of Minho."
+            description="This project consists of a 3D engine and generator developed in C++ and OpenGL."
             :technologies="[ 'devicon-cplusplus-plain colored', 'devicon-opengl-plain colored', 'devicon-cmake-plain colored']"
             giturl="https://github.com/JoaoCoelho2003/CG"
           />
           <ProjectBox
-            :images= '["/engweb/strollBraga.jpg"]'
+            :images= '["/engweb/strollBraga.png"]'
             title="Braga Street Map - Web Engineering Project"
-            description="Braga Street Map is a web application that provides information about the different roads in Braga, Portugal. It was developed using Elixir and Phoenix, and deployed using Docker."
+            description="Braga Street Map is a web application that provides information about the different roads in Braga, Portugal."
             :technologies="['devicon-elixir-plain colored', 'devicon-phoenix-original colored', 'devicon-docker-plain colored', 'devicon-html5-plain colored', 'devicon-tailwindcss-original colored', 'devicon-javascript-plain colored', 'devicon-python-plain colored']"
             giturl="https://github.com/JoaoCoelho2003/EngwebProject"
           />
           <ProjectBox
-            :images= '["/engweb/strollBraga.jpg"]'
+            :images= '["/engweb/strollBraga.png"]'
             title="Braga Street Map - Web Engineering Project"
             description="Braga Street Map is a web application that provides information about the different roads in Braga, Portugal. It was developed using Elixir and Phoenix, and deployed using Docker."
             :technologies="['devicon-elixir-plain colored', 'devicon-phoenix-original colored', 'devicon-docker-plain colored', 'devicon-html5-plain colored', 'devicon-tailwindcss-original colored', 'devicon-javascript-plain colored', 'devicon-python-plain colored']"
