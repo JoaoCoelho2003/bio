d045897383ea4b47c6071dc60259bb8beb6a6a22
jojocoelho <jhcoelho03@gmail.com>
2025-01-24 07:04:47 +0000

    feat: netlify functions

--- netlify/functions/post.js
+++ netlify/functions/post.js
@@ -1,17 +1,16 @@
-const { getContent } = require('@nuxt/content')
-
 exports.handler = async (event, context) => {
-  try {
-    const slug = event.path.split('/').pop()
-    const post = await getContent('blog', slug)
-    return {
-      statusCode: 200,
-      body: JSON.stringify(post)
-    }
-  } catch (error) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ error: 'Post not found' })
+    try {
+      const { getContent } = await import('@nuxt/content')
+      const slug = event.path.split('/').pop()
+      const post = await getContent('blog', slug)
+      return {
+        statusCode: 200,
+        body: JSON.stringify(post)
+      }
+    } catch (error) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: 'Post not found' })
+      }
     }
   }
-}
--- netlify/functions/posts.js
+++ netlify/functions/posts.js
@@ -1,16 +1,15 @@
-const { getCollection } = require('@nuxt/content')
-
 exports.handler = async (event, context) => {
-  try {
-    const posts = await getCollection('blog')
-    return {
-      statusCode: 200,
-      body: JSON.stringify(posts)
-    }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'An error occurred fetching the posts' })
+    try {
+      const { getCollection } = await import('@nuxt/content')
+      const posts = await getCollection('blog')
+      return {
+        statusCode: 200,
+        body: JSON.stringify(posts)
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'An error occurred fetching the posts' })
+      }
     }
   }
-}
