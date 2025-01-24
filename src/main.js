6dc3592ff595798ef61355fc366f345aaa784abe
jojocoelho <jhcoelho03@gmail.com>
2025-01-24 06:38:09 +0000

    feat: added netlify init

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import netlifyIdentity from 'netlify-identity-widget';
 import router from './router'
 import './assets/main.css'
 
+netlifyIdentity.init();
 const app = createApp(App)
 
 app.use(router)
