df9d8a1b67826e1c382e3d3d723457f6beec15de
jojocoelho <jhcoelho03@gmail.com>
2025-03-06 14:04:19 +0000

    fix: main

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import netlifyIdentity from "netlify-identity-widget";
 import router from "./router";
+import { createHead } from "@vueuse/head";
 import "./assets/main.css";
 
 netlifyIdentity.init();
 const app = createApp(App);
+const head = createHead();
 
 app.use(router);
-
+app.use(head);
 app.mount("#app");
