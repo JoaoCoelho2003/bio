347438ebdc161a9677254d1d5d486c64e0be4ede
jojocoelho <jhcoelho03@gmail.com>
2025-07-08 21:27:28 +0100

    fix: app

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,9 @@
 <template>
   <div class="bg-black min-h-screen text-green-500 font-mono">
-    <RouterView v-slot="{ Component }">
-      <Transition name="fade" mode="out-in">
-        <component :is="Component" />
-      </Transition>
-    </RouterView>
+    <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
 </script>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
